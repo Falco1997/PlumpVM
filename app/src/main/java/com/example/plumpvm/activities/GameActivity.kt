2e745f551e98e62117d940041bee7a7e9cdb1240
Falco1997 <filipgolf1997@gmail.com>
2021-06-13 15:23:40 +0200

    Sends size correctly to gameActivity

---
+++ app/src/main/java/com/example/plumpvm/activities/GameActivity.kt
@@ -0,0 +1,32 @@
+package com.example.plumpvm.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.plumpvm.*
+import com.example.plumpvm.databinding.ActivityGameBinding
+
+class GameActivity : AppCompatActivity() {
+
+    companion object {
+        private const val NUMBER_OF_PLAYERS = "NUMBER_OF_PLAYERS"
+        private const val NUMBER_OF_ROUNDS = "NUMBER_OF_ROUNDS"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+        val binding = ActivityGameBinding.inflate(layoutInflater)
+
+        val numberOfPlayers = intent.getStringExtra(NUMBER_OF_PLAYERS)
+        val numberOfRounds = intent.getStringExtra(NUMBER_OF_ROUNDS)
+
+        Log.d("plumpDebug", "GameActivity Players: " + numberOfPlayers.toString())
+        Log.d("plumpDebug", "GameActivity Rounds: " + numberOfRounds.toString())
+
+        binding.test1.text = numberOfPlayers
+        binding.test2.text = numberOfRounds
+
+    }
+}
--- app/src/main/java/com/example/plumpvm/fragments/StartGameFragment.kt
+++ app/src/main/java/com/example/plumpvm/fragments/StartGameFragment.kt
@@ -1,17 +1,21 @@
 package com.example.plumpvm.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.plumpvm.R
+import android.widget.AdapterView
+import com.example.plumpvm.activities.GameActivity
 import com.example.plumpvm.databinding.FragmentStartGameBinding
 
 class StartGameFragment : Fragment() {
 
     companion object {
-
+        private const val NUMBER_OF_PLAYERS = "NUMBER_OF_PLAYERS"
+        private const val NUMBER_OF_ROUNDS = "NUMBER_OF_ROUNDS"
     }
 
     lateinit var binding: FragmentStartGameBinding
@@ -34,7 +38,15 @@ class StartGameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.fragmentStartGameButtonStartGame.setOnClickListener {
+            val startGameIntent = Intent(context, GameActivity::class.java).apply {
+                Log.d("plumpDebug", binding.fragmentStartGameSpinnerNumberOfPlayers.selectedItem.toString())
+                Log.d("plumpDebug", binding.fragmentStartGameSpinnerNumberOfRounds.selectedItem.toString())
+                putExtra(NUMBER_OF_PLAYERS, binding.fragmentStartGameSpinnerNumberOfPlayers.selectedItem.toString())
+                putExtra(NUMBER_OF_ROUNDS, binding.fragmentStartGameSpinnerNumberOfRounds.selectedItem.toString())
+            }
+            startActivity(startGameIntent)
+        }
 
     }
 
